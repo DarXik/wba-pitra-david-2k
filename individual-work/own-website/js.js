00f0085de82d432478a85e9110864de02b000d20
DarXik <davca749@gmail.com>
2022-01-13 17:21:00 +0100

    updates

--- individual-work/own-website/js.js
+++ individual-work/own-website/js.js
@@ -1,4 +1,4 @@
-var i = 0;
+/* var i = 0;
 var txt = "vítejte, hodinářství pantera je zde pro vás";
 var speed = 125;
 function typeWriter(){
@@ -6,4 +6,15 @@ function typeWriter(){
     document.getElementById("welcome").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
-}
+} */
+
+function top() {
+    document.getElementById( 'top' ).scrollIntoView();    
+};
+
+function bottom() {
+    document.getElementById( 'bottom' ).scrollIntoView();
+    window.setTimeout( function () { top(); }, 2000 );
+};
+
+bottom();
