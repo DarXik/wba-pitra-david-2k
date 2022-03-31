bc4a2067b97fed16a8f6a629b4a2870054c345e9
DarXik <73177923+DarXik@users.noreply.github.com>
2022-03-31 09:35:25 +0100

    new js

---
+++ individual-work/Tests/index.js
@@ -0,0 +1,31 @@
+
+var a = null;
+function soucet() {
+    let a = 10;
+    let b = 7;
+    let nasobek = a * b;
+
+    console.log("zbytek je " + (a % b));
+}
+
+/* soucet();
+
+
+let cars = ["BMW", "Mercedes", "Porsche", "Audi"];
+
+cars.forEach((carItem) => {
+    console.log("Položka z pole je " + carItem)
+});
+
+const multiNums = (x, y) => {
+    console.log("Násobek je " + x / y)
+};
+multiNums(99, 0); */
+
+if (a > 9) {
+    console.log("áčko je větší než 9");
+
+} else {
+    console.log("nasrat");
+
+}
