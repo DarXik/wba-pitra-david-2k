c69c364ca92e6a4895df969450bfd54cdf1bdf14
DarXik <davca749@gmail.com>
2022-04-17 17:55:32 +0200

    javascript

--- individual-work/wba-projekt-2/scripts/index.js
+++ individual-work/wba-projekt-2/scripts/index.js
@@ -1,4 +1,16 @@
+var i = 0;
+var txt = 'Beginings';
+var speed = 150;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("js1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
 function actionToggle() {
     const action = document.querySelector('.action');
     action.classList.toggle('active')
-  }
+}
