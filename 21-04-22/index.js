7000598b9f0462fa3e10a084c3a678b073528c15
DarXik <73177923+DarXik@users.noreply.github.com>
2022-04-21 13:02:40 +0200

    edits

--- 21-04-22/index.js
+++ 21-04-22/index.js
@@ -4,3 +4,23 @@ const setNums = () => {
 }
 
 setNums();
+
+document.getElementById("demo").innerHTML = "dom je js";
+
+const click = document.getElementById("demo");
+
+function h1Click() {
+    click.innerHTML = "dom";
+}
+
+/* $(document).ready(() => {
+
+
+
+}); */
+
+$(document).ready(function() {
+    $(p).click(function() {
+        $(h1).hide();
+    });
+});
